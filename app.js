8f444f6cce7e3dadf485bdee33d6d566209d63a7
Ace Alrich <hex0@live.com>
2021-01-26 22:41:42 +0000

    event example

--- app.js
+++ app.js
@@ -2,13 +2,23 @@ const app = Vue.createApp({
   data() {
     return {
       Name: 32,
+      boxOn: false,
     };
   },
   methods: {
     handleEvent(event) {
       console.log(event);
     },
+    handleBoxClick() {
+      this.boxOn = !this.boxOn;
+    },
   },
 });
 
 app.mount("#app");
+
+const theBox = document.getElementById("boxThatWasClicked");
+
+theBox.addEventListener("click", function (event) {
+  event.target.classList.toggle("arch-box-on");
+});
